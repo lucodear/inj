24235495704555674babcf68c2214684433411d3
Lucas Colombo <lucasncolombo@gmail.com>
2025-02-22 16:41:29 -0300

    feat: ✨ remove support for py 3.8, add support for 3.13

--- noxfile.py
+++ noxfile.py
@@ -3,11 +3,11 @@ from nox_poetry import Session, session
 
 @session(
     python=[
+        '3.13',
         '3.12',
         '3.11',
         '3.10',
         '3.9',
-        '3.8',
     ]
 )
 def tests(session: Session) -> None:
